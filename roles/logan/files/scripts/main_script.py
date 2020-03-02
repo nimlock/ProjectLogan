538f9c866aa1b244fe8fbf3f121dde6b6767a5b7
Zhilyaev Ivan <nimlock@yandex.ru>
2020-03-02 18:16:02 +0000

    Put all to Ansible-role

--- roles/logan/files/scripts/main_script.py
+++ roles/logan/files/scripts/main_script.py
@@ -41,9 +41,6 @@ def main_func(file_name):
             lines_counter = lines_counter + 1
             # Make some work if list 'time_plus_lag' not empty
             if parsing_result:
-                # DEBUG # print ('PARSER ACCEPT: ' + line.strip())
-                # DEBUG # print ('Time-code is: ' + parsing_result[0] + '. Lag time is: ' + parsing_result[1])
-                # DEBUG # print ("Lag is over limit!!!" if parsing_result[2] else "Limit not reached.")
                 if parsing_status is 'ok':
                     parsing_status = we_in_status_ok(parsing_result)
                 elif parsing_status is 'pre_alarm':
@@ -53,13 +50,11 @@ def main_func(file_name):
 
             # If parser return nothing (that string of file mismatch with RE) we can do something, or not ))
             else:
-                # DEBUG # print ('PARSER DROP: ' + line)
                 parser_drop_counter = parser_drop_counter + 1
                 pass
 
         # When lines in file ends we have to make final work
         # DEBUG # print('THE END OF THE FILE. Processing ' + str(lines_counter) + ' line(s). Parser drop ' + str(parser_drop_counter) + ' line(s).')
-        # DEBUG # print result_dict
 
     with open(outputdir + 'result_' + file_name, 'w') as result_file:
         if result_dict:
@@ -102,7 +97,6 @@ def we_in_status_ok(parsing_result):
     # If lag is beyond lag_threshold, then change status:
     if parsing_result[2]:
         statistic_dict = {1: parsing_result}
-        # DEBUG # print statistic_dict
         # DEBUG # print ('Changing status to PRE_ALARM!')
         return 'pre_alarm'
     # If everything is fine, then just do nothing
@@ -113,9 +107,7 @@ def we_in_status_ok(parsing_result):
 def we_in_status_pre_alarm(parsing_result):
     global statistic_dict, alarm_dict
     count_not_ok = int(max(statistic_dict.keys())) + 1
-    # DEBUG # print ('Count_not_ok for now = ' + str(count_not_ok))
     statistic_dict[count_not_ok] = parsing_result
-    # DEBUG # print statistic_dict
 
     if count_not_ok >= criteria[0]:
         # Getting quantity of lines with high lag for some last lines (criteria[0]):
@@ -137,12 +129,10 @@ def we_in_status_pre_alarm(parsing_result):
                 if statistic_dict.get(i)[2]:
                     alarm_start = i
                     break
-            # DEBUG # print ('alarm_start = ' + str(alarm_start))
 
             # Fill alarm_dict:
             alarm_dict = statistic_dict.copy()
             for key in range(1, alarm_start):
-                # DEBUG # print ('key = ' + str(key))
                 del alarm_dict[key]
             if not alarm_dict.get(1):
                 for another_key in range(alarm_start, len(statistic_dict) + 1):
@@ -150,11 +140,6 @@ def we_in_status_pre_alarm(parsing_result):
 
             # Clear statistic_dict:
             statistic_dict.clear()
-
-            # DEBUG # print ('Alarm_dict:')
-            # DEBUG # print alarm_dict
-            # DEBUG # print ('Statistic_dict (should be empty):')
-            # DEBUG # print statistic_dict
 
             return 'alarm'
 
@@ -166,9 +151,7 @@ def we_in_status_pre_alarm(parsing_result):
 def we_in_status_alarm(parsing_result):
     global alarm_dict, result_dict
     count_not_ok = int(max(alarm_dict.keys())) + 1
-    # DEBUG # print ('Count for now = ' + str(count_not_ok))
     alarm_dict[count_not_ok] = parsing_result
-    # DEBUG # print statistic_dict
 
     if count_not_ok >= criteria[0]:
 
@@ -182,11 +165,6 @@ def we_in_status_alarm(parsing_result):
         else:
             # Alarm is over.
 
-            # DEBUG # print ('\nAlarm is over. Here is full alarm_dict:')
-            # DEBUG # print alarm_dict
-            # DEBUG # print ('We will check next range of dict for cutting:')
-            # DEBUG # print range(len(alarm_dict), len(alarm_dict) - criteria[0], -1)
-
             # Cut good lines from the end of alarm_dict.
             for i in range(len(alarm_dict), len(alarm_dict) - criteria[0], -1):
 
@@ -194,10 +172,6 @@ def we_in_status_alarm(parsing_result):
                     del alarm_dict[i]
                 else:
                     break
-
-            # DEBUG # print ('And here is cuted alarm_dict:')
-            # DEBUG # print ('\n')
-            # DEBUG # print alarm_dict
 
             # We need to find time of incident, it duration, quantity of respective lines in log and max value of lag
 
@@ -215,13 +189,6 @@ def we_in_status_alarm(parsing_result):
             incident_max_lag = max(calc_max_lag_list)
 
             # We need to write statistics about it.
-            # DEBUG # print ('__________WRITING SOME STATISTIC ABOUT ALARM__________')
-            # DEBUG # print ('incident_begin ' + incident_begin)
-            # DEBUG # print ('incident_first_logline ' + incident_first_logline)
-            # DEBUG # print ('incident_end ' + incident_end)
-            # DEBUG # print ('incident_duration ' + str(incident_duration))
-            # DEBUG # print ('incident_chain ' + str(incident_chain))
-            # DEBUG # print ('incident_max_lag ' + str(incident_max_lag))
 
             if not result_dict:
                 result_count = 1
@@ -245,7 +212,6 @@ def overlimits_lags_counter(count_not_ok, dict_to_count):
     for i in keys_to_request:
         if dict_to_count.get(i)[2]:
             number_of_overlags = number_of_overlags + 1
-    # DEBUG # print ('Overlags = ' + str(number_of_overlags))
     return number_of_overlags
 
 
@@ -265,6 +231,5 @@ while True:
     files_list = os.listdir(inputdir)
     for single_file in files_list:
         if single_file is not None:
-            # DEBUG # print('Find some files in Input: ' + single_file)
             main_func(single_file)
     time.sleep(t)
